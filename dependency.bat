aa9aba711dafa188fe8c63c77636437e419d745b
Abhishek <abhisheknodejsdeveloper@gmail.com>
2025-05-19 22:13:31 +0530

    Create dependency.bat

---
+++ dependency.bat
@@ -0,0 +1,17 @@
+echo Install all npm
+pause
+echo bcrypt 
+echo cookie-parser
+echo ejs
+echo express
+echo jsonwebtoken
+echo mongodb
+echo mongoose
+echo nodemon
+echo path
+echo validator
+pause
+cls
+npm install bcrypt cookie-parser ejs express jsonwebtoken mongodb mongoose path validator
+pause
+exit
